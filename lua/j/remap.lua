9888c0a181402e3d50d404be9caa2561a9babe81
Julian Nymark <julian.nymark@nav.no>
2024-04-30 00:25:14 +0200

    add remaps (without recursion)

--- lua/j/remap.lua
+++ lua/j/remap.lua
@@ -26,6 +26,9 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { remap = false })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { remap = false })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
