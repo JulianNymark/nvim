66ca14ed57de8585164b72d96d89ed8bf2a24923
Julian Nymark <j@juliannymark.com>
2024-05-01 00:41:17 +0200

    :sparkles: some LSP setup

--- after/plugin/nvim-lspconfig.lua
+++ after/plugin/nvim-lspconfig.lua
@@ -1,25 +1,3 @@
--- Brief aside: **What is LSP?**
---
--- LSP is an initialism you've probably heard, but might not understand what it is.
---
--- LSP stands for Language Server Protocol. It's a protocol that helps editors
--- and language tooling communicate in a standardized fashion.
---
--- In general, you have a "server" which is some tool built to understand a particular
--- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
--- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
--- processes that communicate with some "client" - in this case, Neovim!
---
--- LSP provides Neovim with features like:
---  - Go to definition
---  - Find references
---  - Autocompletion
---  - Symbol Search
---  - and more!
---
--- Thus, Language Servers are external tools that must be installed separately from
--- Neovim. This is where `mason` and related plugins come into play.
---
 -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
 -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
@@ -139,18 +117,16 @@ capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp"
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 local servers = {
 	-- clangd = {},
-	-- gopls = {},
-	-- pyright = {},
-	-- rust_analyzer = {},
+	gopls = {},
+	pyright = {},
+	rust_analyzer = {},
 	-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 	--
 	-- Some languages (like typescript) have entire language plugins that can be useful:
 	--    https://github.com/pmizio/typescript-tools.nvim
 	--
 	-- But for many setups, the LSP (`tsserver`) will work just fine
-	-- tsserver = {},
-	--
-
+	tsserver = {},
 	lua_ls = {
 		-- cmd = {...},
 		-- filetypes = { ...},
