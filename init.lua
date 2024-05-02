afd836320a7770ff5ad080a706a06039d5e4f00b
Julian Nymark <julian.nymark@nav.no>
2024-05-02 12:10:02 +0200

    :sparkles: cssls and some gitsigns keymaps

--- init.lua
+++ init.lua
@@ -111,6 +111,63 @@ require("lazy").setup({
 				changedelete = { text = "~" },
 			},
 		},
+		config = function(_, opts)
+			local gitsigns = require("gitsigns")
+			gitsigns.setup(vim.tbl_deep_extend("force", {
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end)
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end)
+
+					-- Actions
+					map("n", "<leader>hs", gitsigns.stage_hunk)
+					map("n", "<leader>hr", gitsigns.reset_hunk)
+					map("v", "<leader>hs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("v", "<leader>hr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("n", "<leader>hS", gitsigns.stage_buffer)
+					map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+					map("n", "<leader>hR", gitsigns.reset_buffer)
+					map("n", "<leader>hp", gitsigns.preview_hunk)
+					map("n", "<leader>hb", function()
+						gitsigns.blame_line({ full = true })
+					end)
+					map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+					map("n", "<leader>hd", gitsigns.diffthis)
+					map("n", "<leader>hD", function()
+						gitsigns.diffthis("~")
+					end)
+					map("n", "<leader>td", gitsigns.toggle_deleted)
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			}, opts))
+		end,
 	},
 
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -453,9 +510,8 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
-				--
-
+				tsserver = {},
+				cssls = {},
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
